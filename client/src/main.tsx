a7bcdb716e2a1dd08ba9584977b6ff1ddec340d6
Elliot <elskieproduction@gmail.com>
2025-07-05 21:18:24 +0100

    chore

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
+import App from "./App.jsx";
 import "./index.css";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
