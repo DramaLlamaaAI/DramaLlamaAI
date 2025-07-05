34406cda1970db45b7e7f7f339198ff9506135ea
Elliot <elskieproduction@gmail.com>
2025-07-05 21:10:23 +0100

    refactor: convert App component from TypeScript to JavaScript

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,7 @@ function Router() {
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-function Layout({ children }: { children: React.ReactNode }) {
+function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
